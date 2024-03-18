2d59aa3765c9b3bd3d283300419849f053a81a14
pantanick <pantazisn2@gmail.com>
2024-03-18 01:04:44 +0100

    Reformating

--- hello/main.cc
+++
@@ -1,5 +0,0 @@
-#include<stdio.h>
-int main () {
-	printf("hello from c++, everybody\n");
-	return 0;
-}
--- hello/main.cs
+++
@@ -1,8 +0,0 @@
-class main
-{
-  static int Main ()
-  {
-    System.Console.Write ("hello, everybody\n");
-    return 0;
-  }
-}
